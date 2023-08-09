d7a7a67a459eda34a26441fb180e5e247cfcaefc
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-08-09 13:28:39 -0500

    Re-attempt simple todo project

--- notes/06-simple-todo-list/src/App.jsx
+++ notes/06-simple-todo-list/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './styles.css'
 import Item from './Item'
 
 function App() {
-
-  const [inputText, setInputText] = useState('')
+  const [todoName, setTodoName] = useState('')
   const [todoList, setTodoList] = useState([])
 
-  // useEffect(() => {
-  //   listItem = todoList.map(item => {
-  //     return <li>item</li>
-  //   })
-  // }, [todoList])
-
   function addTodo() {
-    if (inputText === '') return
     setTodoList(currentList => {
-      return [...currentList,
-      { name: inputText, completed: false, id: crypto.randomUUID() }]
+      return [...currentList, {id: crypto.randomUUID(), name: todoName, checked: false}]
     })
-    setInputText('')
-    console.log(todoList)
+    setTodoName('')
   }
 
-  function toggleTodo(id, completed) {
+  function toggleTodo(id, checked) {
     setTodoList(currentList => {
-      return currentList.map(item => {
-        if (item.id === id) {
-          return {...item, completed: completed}
+      return currentList.map(listItem => {
+        if (listItem.id === id) {
+          return {...listItem, checked: checked}
         } else {
-          return item
+          return listItem
         }
       })
     })
@@ -37,26 +27,33 @@ function App() {
 
   function deleteTodo(id) {
     setTodoList(currentList => {
-      return currentList.filter(item => item.id !== id)
+      return currentList.filter(item => {
+        if (item.id !== id) {
+          return item
+        }
+        return 
+      })
+    })
+
+    todoList.map(listItem => {
+      return listItem.filter()
+    })
+
+    todoList.filter(listItem => {
+      return listItem.id !== id
     })
   }
-
-
 
   return (
     <>
       <ul id="list">
-        {/* {todoList.map(item => {
-          <Item name={item.name} completed={item.completed} key={item.id} />
-        })} */}
-
-        {todoList.map(item =>
-          <Item key={item.id} {...item} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
-        )}
+        {todoList.map(listItem => {
+          return <TodoItem key={listItem.id} {...listItem} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+        })}
       </ul>
-      <div id='new-todo-form'>
-        <label htmlFor="todo-input">New Todo</label>
-        <input id="todo-input" type="text" value={inputText} onChange={event => setInputText(event.target.value)}/>
+      <div id="new-todo-form">
+        <label htmlFor="new-todo">New Todo</label>
+        <input type="text" id="new-todo" value={todoName} onChange={e => setTodoName(e.target.value)} />
         <button onClick={addTodo}>Add Todo</button>
       </div>
     </>
--- notes/06-simple-todo-list/src/Item.jsx
+++ notes/06-simple-todo-list/src/Item.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import './styles.css'
 
-function Item({id, name, completed, toggleTodo, deleteTodo}) {
+function Item({id, name, checked, toggleTodo, deleteTodo}) {
   return (
-    <li className="list-item" key={id}>
+    <li key={id} className="list-item">
       <label className="list-item-label">
-        <input type="checkbox" checked={completed} onChange={e => toggleTodo(id, e.target.checked)} data-list-item-checkbox />
-        <span data-list-item-text>{name}</span>
+        <input type="checkbox" checked={checked} onChange={e => toggleTodo(id, e.target.checked)} data-list-item-checkbox/>
+        <span data-list-item-text >{name}</span>
       </label>
-      <button onClick={() => deleteTodo(id)} data-button-delete>Delete</button>
+      <button onClick={() => deleteTodo(id)}data-button-delete>Delete</button>
     </li>
   )
 }
